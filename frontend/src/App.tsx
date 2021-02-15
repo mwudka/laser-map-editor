fdc3a030d767b0713dd65162a48e5626312edbd5
Manuel Wudka-Robles <mwudka@me.com>
2021-02-14 16:58:24 -0800

    Port minimal-mvt mapbox example to react app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,79 @@
+import { useState, useRef, useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
 import './App.css';
 
 function App() {
+  const mapContainer = useRef<HTMLDivElement>(null);
+  const [lng, setLng] = useState(-74.006);
+  const [lat, setLat] = useState(40.71);
+  const [zoom, setZoom] = useState(16);
+
+  useEffect(() => {
+    console.log('Creating mapboxgl map');
+    const map = new mapboxgl.Map({
+      container: mapContainer.current!,
+      center: [lng, lat],
+      zoom: zoom,
+      minZoom: 16,
+      style: {
+        'version': 8,
+        'sources': {
+            'postgis-tiles': {
+                'type': 'vector',
+                'tiles': [
+                    "http://localhost:8082/{z}/{x}/{y}"
+                ]
+            }
+
+        },
+        'layers': [{
+            'id': 'postgis-tiles-layer',
+            'type': 'line',
+            'source': 'postgis-tiles',
+            // ST_AsMVT() uses 'default' as layer name
+            'source-layer': 'default', 
+            'filter': 
+                ["has", "highway"]
+            ,
+            'minzoom': 16,
+            'maxzoom': 22,
+            'paint': {
+                'line-opacity': 0.7,
+                'line-color': 'red',
+                'line-width': 1
+            }
+        },
+        {
+            'id': 'postgis-tiles-layer2',
+            'type': 'fill',
+            'source': 'postgis-tiles',
+            // ST_AsMVT() uses 'default' as layer name
+            'source-layer': 'default', 
+            'filter': 
+                ["has", "building"]
+            ,
+            'minzoom': 16,
+            'maxzoom': 22,
+            'paint': {
+                'fill-color': '#ffffff'
+            }
+        }
+    ]
+    }
+    });
+    map.on("move", () => {
+      setLng(map.getCenter().lng);
+      setLat(map.getCenter().lat);
+      setZoom(map.getZoom());
+    });
+    
+    // We only want this to run once. React docs say that empty deps array means only run effect hook once.
+    // eslint-disable-next-line 
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={mapContainer} className="mapContainer">
+      
     </div>
   );
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN!;
 
 ReactDOM.render(
   <React.StrictMode>
