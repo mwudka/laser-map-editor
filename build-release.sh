ad02fc102738f47b0d0ad2db55a3e68ed9d004d2
Manuel Wudka-Robles <mwudka@gmail.com>
2021-05-29 13:47:27 -0700

    Statically link release builds

--- build-release.sh
+++ build-release.sh
@@ -6,4 +6,4 @@ set -eux
 cd tileserver
 
 (cd frontend && npm ci && npm run build)
-GOOS=linux GOARCH=amd64 go build -o tileserver-linux-amd64
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o tileserver-linux-amd64
