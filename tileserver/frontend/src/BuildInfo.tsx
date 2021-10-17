219b0cd7736144ae1d2c03d08ab4d67cfcfb83e7
Manuel Wudka-Robles <mwudka@me.com>
2021-10-16 17:14:16 -0700

    Tweak buildinfo style

--- tileserver/frontend/src/BuildInfo.tsx
+++ tileserver/frontend/src/BuildInfo.tsx
@@ -2,5 +2,5 @@ import './BuildInfo.css';
 
 export default function Exporter() {
     
-    return <span className="buildInfo">{process.env.REACT_APP_BUILD_INFO || 'dev-build'}</span>
+    return <span className="buildInfo">Version {process.env.REACT_APP_BUILD_INFO || 'dev-build'}</span>
 }
